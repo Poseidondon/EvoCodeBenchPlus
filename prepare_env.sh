4d0dc49e8a2231c2a16495deb4e162a54e13aa9e
Poseidondon <b.zarubin@innopolis.university>
2025-02-21 19:55:29 +0300

    fixing dependencies v1

---
+++ prepare_env.sh
@@ -0,0 +1,3 @@
+sudo apt-get install build-essential
+sudo apt-get isntall libx11-devel
+sudo apt-get install xorg-dev libglu1-mesa-dev
--- setup_env.sh
+++ setup_env.sh
@@ -1,19 +1,46 @@
-Root=/home/k1shin/EvoCodeBench
-Data_Path=$Root/data.jsonl
-Source_Code_Root=$Root/Source_Code
+root=/home/k1shin/Projects/EvoCodeBenchPlus
+data_path=$root/dataset/data/data.jsonl
+repos_dir=$root/dataset/repos
+venv_dir="$root/dataset/venvs"
 
 
-# Setup the execution environment for contrastors
-repo_name="contrastors"
-venv_dir="$Root/venvs/$repo_name"
-# Check if the virtual environment directory already exists
-if [ -d "$venv_dir" ]; then
-  echo "Virtual environment already exists for $repo_name at $venv_dir. Skipping creation."
+# repo_name="contrastors"
+# venv_path="$venv_dir/$repo_name"
+# if [ -d "$venv_path" ]; then
+#   echo "Virtual environment already exists for $repo_name at $venv_path. Skipping creation."
+# else
+#   echo "Creating virtual environment for $repo_name at $venv_path..."
+#   python -m venv $venv_path && source $venv_path/bin/activate
+#   cd $repos_dir/$repo_name || exit
+#   pip install pytest pytest-runner
+#   pip install -r requirements.txt
+#   deactivate
+# fi
+
+
+repo_name="EasyVolcap"
+venv_path="$venv_dir/$repo_name"
+if [ -d "$venv_path" ]; then
+  echo "Virtual environment already exists for $repo_name at $venv_path. Skipping creation."
 else
-  # Create the virtual environment if it doesn't exist
-  echo "Creating virtual environment for $repo_name at $venv_dir..."
-  python -m venv $venv_dir && source $venv_dir/bin/activate
-  cd $Root/Source_Code/$repo_name || exit
+  echo "Creating virtual environment for $repo_name at $venv_path..."
+  python -m venv $venv_path && source $venv_path/bin/activate
+  cd $repos_dir/$repo_name || exit
+  pip install pytest pytest-runner
+  pip install torch
+  pip install -r requirements.txt
+  deactivate
+fi
+
+
+repo_name="microagents"
+venv_path="$venv_dir/$repo_name"
+if [ -d "$venv_path" ]; then
+  echo "Virtual environment already exists for $repo_name at $venv_path. Skipping creation."
+else
+  echo "Creating virtual environment for $repo_name at $venv_path..."
+  python -m venv $venv_path && source $venv_path/bin/activate
+  cd $repos_dir/$repo_name || exit
   pip install pytest pytest-runner
   pip install -r requirements.txt
   deactivate
