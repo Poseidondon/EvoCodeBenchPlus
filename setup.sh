3f1aa6f82d80bae9f8b0b3a3ea2ddb8e3a903a4d
KonstFed <konstantin_fedorov_03@mail.ru>
2026-02-26 18:04:03 +0800

    simplify setup.sh for docker

--- setup.sh
+++ setup.sh
@@ -3,25 +3,12 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# conda environment
-# conda create --name EvoCodeBench python=3.11
-# conda activate EvoCodeBench
-pip install -r requirements.txt
 # load repositories
 echo "Loading dataset"
 if [ -f bash/load_data.sh ]; then
     bash bash/load_data.sh
 else
     echo "Error: load_data.sh not found"
-    exit 1
-fi
-
-# Setup virtual environments
-echo "Setting up virtual environments..."
-if [ -f bash/prepare_env.sh ]; then
-    bash bash/prepare_env.sh
-else
-    echo "Error: prepare_env.sh not found" >&2
     exit 1
 fi
 
